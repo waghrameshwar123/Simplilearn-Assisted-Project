6b166967d42ff0e2d1f2b995c969743948c64629
Rameshwar Waghmare <waghrameshwar123@gmail.com>
2022-03-18 15:46:37 +0530

    Assisted Project

--- Phase 2 Assisted Practice Project/UserLogin/src/main/java/com/logout.java
+++ Phase 2 Assisted Practice Project/UserLogin/src/main/java/com/logout.java
@@ -3,22 +3,21 @@ package com;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ServletLogin
+ * Servlet implementation class logout
  */
-public class ServletLogin extends HttpServlet {
+public class logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletLogin() {
+    public logout() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,22 +28,10 @@ public class ServletLogin extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("text/html");
 		PrintWriter pw = response.getWriter();
-		String emailid = request.getParameter("emailid");
-		String password = request.getParameter("password");
-	//	RequestDispatcher rd1 = request.getRequestDispatcher("Home");
-		
-			if(emailid.equals("ram@gmail.com") && password.equals("123")){	
-				pw.println("Successfully Login<br>");
-			
-				pw.println("<br /> You are now Logged in </h2><br>");
-				
-				pw.println("<h3><a href = 'logout'>Logout</a></h3>");
-			}else {
-				pw.println("Not Successfully Login");
-			}
-		
+		response.setContentType("text/html");
+		pw.println("Log out Successful<br>");
+		pw.println("<a href = 'index.html'>Login again</a>");
 	}
 
 	/**
